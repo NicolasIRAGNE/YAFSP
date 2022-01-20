97cb4e9ce4ef8309d5d01a2f4878bcd4a55a36a3
Nicolas IRAGNE <nicolas.iragne@alyce.fr>
2022-01-20 18:52:41 +0100

    Should be working now, needs testing

--- main.lua
+++ main.lua
@@ -1,18 +1,18 @@
 local frame = CreateFrame("FRAME", "YAFSPFrame");
 frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
 
-
 local playerGUID = UnitGUID("player")
 
 local function eventHandler(self, event, ...)
     if (event == "COMBAT_LOG_EVENT_UNFILTERED") then
+        -- print message
+        local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName,
+            destFlags, destRaidFlags = CombatLogGetCurrentEventInfo();
         if (subevent == "SPELL_AURA_APPLIED" and destGUID == playerGUID) then
             local spellId, spellName = select(12, CombatLogGetCurrentEventInfo()) -- i hate lua
             -- Check if aura applied is Runic Affinity
-            if (spellId == 354964)
-                then
-                    PlaySoundFile("Interface/AddOns/YAFSP/media/good_music.mp3", "SFX") -- Play in SFX channel so it still plays even if music is muted
-                end
+            if (spellId == 354964) then
+                PlaySoundFile("Interface/AddOns/YAFSP/media/good_music.mp3", "SFX") -- Play in SFX channel so it still plays even if music is muted
             end
         end
     end
